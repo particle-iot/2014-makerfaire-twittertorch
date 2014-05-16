88703d1fc9a72e965ef4f6a0dfae7c092674ef41
Zachary Crockett <towynlin@gmail.com>
2014-05-16 12:24:18 -0700

    Make Spark request as JSON

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var spark_api_options = {
   method: 'POST',
   path: '/v1/devices/' + process.env.twittertorch_deviceid + '/message',
   headers: {
-    'Authorization': 'Bearer ' + process.env.twittertorch_sparktoken
+    'Authorization': 'Bearer ' + process.env.twittertorch_sparktoken,
+    'Content-Type': 'application/json'
   }
 };
 var Twit = require('twit');
@@ -17,13 +18,16 @@ var twitter = new Twit({
 var stream = twitter.stream('statuses/filter', { track: '@sparkdevices #makerfaire' });
 stream.on('tweet', function(tweet){
   console.log('TWEET: ' + tweet.text);
+  var t = tweet.text.replace(/#/g, ''); ///[^a-z0-9 ]/gi, '');
+  console.log('STRIPPED: ' + t);
+
   var req = https.request(spark_api_options, function(res){
     console.log('SPARK: status ' + res.statusCode);
     res.on('data', function(d){
       console.log('SPARK: ' + d);
     });
   });
-  req.write('d=' + encodeURIComponent(tweet.text));
+  req.write(JSON.stringify({t:t}));
   req.end();
   req.on('error', function(e){
     console.error('SPARK: error ' + e);
