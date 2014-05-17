15151a93bfbf5e650823b6529d75a3164507ad36
Zachary Crockett <towynlin@gmail.com>
2014-05-16 22:21:21 -0700

    Flare up

--- pebble/app.js
+++ pebble/app.js
@@ -1,3 +1,6 @@
+var DEVICE_ID = 'YOUR DEVICE ID HERE';
+var ACCESS_TOKEN = 'YOUR ACCESS TOKEN HERE';
+
 simply.on('singleClick', function(e) {
   console.log(util2.format('single clicked $button!', e));
   var mode = 1;
@@ -9,7 +12,7 @@ simply.on('singleClick', function(e) {
   }
   ajax(
     {
-      url: 'https://api.spark.io/v1/devices/{{DEVICE_ID}}/params?access_token={{ACCESS_TOKEN}}',
+      url: 'https://api.spark.io/v1/devices/' + DEVICE_ID + '/params?access_token=' + ACCESS_TOKEN,
       method: 'post',
       type: 'json',
       data: { m: 'mode=' + mode }
@@ -17,6 +20,40 @@ simply.on('singleClick', function(e) {
     function(e){ console.log('ajax success: ' + e); },
     function(e){ console.log('ajax failure: ' + e); }
   );
+});
+
+var flame_up = function() {
+  var flame_down = function () {
+    ajax(
+      {
+        url: 'https://api.spark.io/v1/devices/' + DEVICE_ID + '/params?access_token=' + ACCESS_TOKEN,
+        method: 'post',
+        type: 'json',
+        data: { m: 'up_rad=40' }
+      },
+      function(e){ console.log('ajax success: ' + e); },
+      function(e){ console.log('ajax failure: ' + e); }
+    );
+  };
+
+  ajax(
+    {
+      url: 'https://api.spark.io/v1/devices/' + DEVICE_ID + '/params?access_token=' + ACCESS_TOKEN,
+      method: 'post',
+      type: 'json',
+      data: { m: 'up_rad=150' }
+    },
+    function(e){
+      console.log('ajax success: ' + e);
+      setTimeout(flame_down, 1500);
+    },
+    function(e){ console.log('ajax failure: ' + e); }
+  );
+};
+
+simply.on('accelTap', function(e) {
+  console.log('accelTap ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+  flame_up();
 });
 
 simply.setText({
