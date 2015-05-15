2ea9e43430c9e20e2738a1ddd8057e006d4f7ddf
Zachary Crockett <towynlin@gmail.com>
2015-05-14 21:08:48 -0700

    Change spark to particle

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,10 +1,10 @@
 var https = require('https');
-var spark_api_options = {
-  hostname: 'api.spark.io',
+var particle_api_options = {
+  hostname: 'api.particle.io',
   method: 'POST',
   path: '/v1/devices/' + process.env.twittertorch_deviceid + '/message',
   headers: {
-    'Authorization': 'Bearer ' + process.env.twittertorch_sparktoken,
+    'Authorization': 'Bearer ' + process.env.twittertorch_particletoken,
     'Content-Type': 'application/json'
   }
 };
@@ -15,21 +15,21 @@ var twitter = new Twit({
   access_token:         process.env.twittertorch_twittertoken,
   access_token_secret:  process.env.twittertorch_twittertokensecret
 });
-var stream = twitter.stream('statuses/filter', { track: '@sparkdevices #makerfaire' });
+var stream = twitter.stream('statuses/filter', { track: '@particle #makerfaire' });
 stream.on('tweet', function(tweet){
   console.log('TWEET: ' + tweet.text);
   var t = tweet.text.replace(/#/g, ''); ///[^a-z0-9 ]/gi, '');
   console.log('STRIPPED: ' + t);
 
-  var req = https.request(spark_api_options, function(res){
-    console.log('SPARK: status ' + res.statusCode);
+  var req = https.request(particle_api_options, function(res){
+    console.log('PARTICLE: status ' + res.statusCode);
     res.on('data', function(d){
-      console.log('SPARK: ' + d);
+      console.log('PARTICLE: ' + d);
     });
   });
   req.write(JSON.stringify({t:t}));
   req.end();
   req.on('error', function(e){
-    console.error('SPARK: error ' + e);
+    console.error('PARTICLE: error ' + e);
   });
 });
